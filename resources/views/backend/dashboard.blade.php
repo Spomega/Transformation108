6987f2e42e1436fdabf226b8d3903d7b1709dd5b
joseph.davis <joseph.davis@thinksurance.de>
2021-01-25 22:25:29 +0100

    first commit

---
+++ resources/views/backend/dashboard.blade.php
@@ -0,0 +1,27 @@
+@extends('backend.layouts.app')
+
+@section('title', __('Dashboard'))
+
+@section('content')
+    <x-backend.card>
+        <x-slot name="header">
+            @lang('Welcome :Name', ['name' => $logged_in_user->name])
+        </x-slot>
+
+        <x-slot name="body">
+            @lang('Welcome to the Dashboard')
+            <div class="row clearfix">
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                    <div class="info-box bg-pink hover-expand-effect">
+                        <div class="icon">
+                            <i class="material-icons">playlist_add_check</i>
+                        </div>
+                        <div class="content">
+                            <div class="text">NEW TASKS</div>
+                            <div class="number count-to" data-from="0" data-to="125" data-speed="15" data-fresh-interval="20"></div>
+                        </div>
+                    </div>
+                </div>
+        </x-slot>
+    </x-backend.card>
+@endsection
